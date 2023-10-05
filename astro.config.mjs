cc68a7589e069774aacaaad547caa60861f40440
Sam Liu <hello@samliu.dev>
2023-10-05 18:48:48 -0400

    Add Svelte and Tailwind Integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
-
 import vercel from "@astrojs/vercel/serverless";
+import svelte from "@astrojs/svelte";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: vercel({
-    imageService: true,
+    imageService: true
   }),
+  integrations: [svelte(), tailwind()]
 });
