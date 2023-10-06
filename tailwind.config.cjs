7155f9417f6e505f65086b919e0ff92350e887fd
Sam Liu <hello@samliu.dev>
2023-10-05 20:52:03 -0400

    First iteration website ship 🚀

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,17 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'light-trillium-green': '#edfff7',
+				'trillium-yellow': '#ffffb4',
+				'trillium-green': '#2e9568'
+			},
+			fontFamily: {
+				'wack': ['Wack Club Sans', 'sans-serif'],
+				'josefin': ['Josefin Sans', 'sans-serif']
+			}
+		},
 	},
 	plugins: [],
 }
